1d0a2374fef73cbb5eaa11dafb7213f3f934cabe
Vetusia <ivplportfolio@gmail.com>
2021-12-09 19:37:48 +0300

    add public

---
+++ js/script.js
@@ -0,0 +1,57 @@
+/* components */
+var swiper = new Swiper(".js-banner__swiper", {  
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+let btnMobMenu = document.querySelector('.btn-mob');
+let headerRight = document.querySelector('.header__right');
+
+btnMobMenu.addEventListener('click', function() {  
+    this.classList.toggle('btn-mob--open');
+
+    if(!(headerRight.classList.contains('header__right--open'))) {
+        headerRight.classList.add('header__right--open');
+    } else {
+        headerRight.classList.remove('header__right--open'); 
+    }
+});
+
+
+
+//close mob menu on click to outside block
+// document.addEventListener('click', function(event) {  
+//     if(!headerRight.contains(event.target)) { 
+//         console.log(event.target)
+//         headerRight.classList.remove('header__right--open');
+//         btnMobMenu.classList.remove('btn-mob--open');
+//     }
+// });
+
+
+//close mob menu on resize
+function closeResizeMobMenu() {
+    let screenWidth = document.documentElement.clientWidth;
+
+    let positionTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+    if(screenWidth > 1023) {
+        btnMobMenu.classList.remove('btn-mob--open');
+
+        headerRight.classList.remove('header__right--open');
+    }
+}
+
+window.addEventListener('resize', function(event) {
+    closeResizeMobMenu();
+
+}, true);
